a0ec21d6598707ef96cb2c1ade536b8caf2b56da
james-monding <james.cipmonding@gmail.com>
2026-02-08 10:53:05 +1300

    Delete 04_Python/04_Python directory

--- 04_Python/04_Python/01_ETL_Build_SQLite_FactPrices.py
+++
@@ -1,8 +0,0 @@
-"""
-01_ETL_Build_SQLite_FactPrices.py
-
-Automates extraction, cleaning, and loading (ETL) of daily stock price data.
-Reads multiple CSV files, infers ticker symbols, validates columns,
-and loads cleaned data into a SQLite data warehouse table (fact_prices)
-with primary keys and indexes for analytical querying.
-"""
